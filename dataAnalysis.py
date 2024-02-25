873ff4dbe74b411069ff34c30c64bc933a844594
Ashley DeMott <ashley.demott28@gmail.com>
2024-02-24 22:08:00 -0700

    Added more graphs and tables

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -55,14 +55,35 @@ def get_year_num_games(vg_sales):
     #  and make sure Year column is integers and not float
     year_games = vg_sales[["Year", "Name"]].dropna().astype({"Year": int})
 
-    # Grouping by Year, count the number of games per Year
-    year_games = year_games.groupby("Year").count()
+    # Get the years and the number of names for each year
+    year_games = pd.DataFrame({"Year": year_games["Year"].unique(), "Num Games": year_games.groupby("Year").size() })
 
-    # Rename and sort the values by global sales (rename to clarify database's lower limit)
-    year_games = year_games.rename(columns={"Year": "Year", "Name": "Games with 100k sales"})
-    year_games = year_games.sort_values("Games with 100k sales", ascending=False)
+    # Drop the indicies (groupby adds year as index)
+    year_games = year_games.reset_index(drop=True)
+
+    # Sort the years by the number of games
+    year_games = year_games.sort_values("Year", ascending=False)
 
     return year_games
+
+def get_year_num_games_million(vg_sales):
+    """ Gets the number of games that sold 1 million copies, by release year"""
+    # Using only the Year and Genre columns, drop rows with N/A
+    #  and make sure Year column is integers and not float
+    year_games = vg_sales[["Year", "Global_Sales"]].dropna().astype({"Year": int})
+
+    year_games_million = year_games[year_games["Global_Sales"] > 1.0]
+
+    # Get the years and the number of names for each year
+    year_games_million = pd.DataFrame({"Year": year_games_million["Year"].unique(), "Num Games": year_games_million.groupby("Year").size() })
+
+    # Drop the indicies (groupby adds year as index)
+    year_games_million = year_games_million.reset_index(drop=True)
+
+    # Sort the years by the number of games
+    year_games_million = year_games_million.sort_values("Year", ascending=False)
+
+    return year_games_million
 
 def get_genre_year(vg_sales):
     """ Get the most popular genre per year with how many games sold 100k+"""
@@ -82,7 +103,7 @@ def get_genre_year(vg_sales):
     return best_year_genre
 
 def graph_global_sales(year_global_sales):
-    """ Graphs the global sales by year"""        
+    """Graphs the total global sales by year"""        
     # Data to be graphed
     x = year_global_sales["Year"]
     y = year_global_sales["Global Sales (in millions)"]
@@ -100,6 +121,42 @@ def graph_global_sales(year_global_sales):
     # Show the plot
     plt.show()
 
+def graph_game_per_year(year_games):
+    """Graphs the number of games selling 100k+ copies, by release Year"""
+    # Data to be graphed
+    x = year_games["Year"]
+    y = year_games["Num Games"]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    # Add titles and axis labels
+    fig.suptitle("Video Games Selling 100k copies", fontsize=14, fontweight="bold")
+
+    plt.xlabel("Release Year")
+    plt.ylabel("Number of Games")
+
+    # Show the plot
+    plt.show()
+
+def graph_game_per_year_million(year_games_million):
+    """Graphs the number of games selling 1M+ copies, by release year"""   
+    # Data to be graphed
+    x = year_games_million["Year"]
+    y = year_games_million["Num Games"]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    # Add titles and axis labels
+    fig.suptitle("Video Games Selling 1 Million copies", fontsize=14, fontweight="bold")
+
+    plt.xlabel("Release Year")
+    plt.ylabel("Number of Games")
+
+    # Show the plot
+    plt.show()
+
 def main():
     vg_sales = get_data_set()
 
@@ -108,29 +165,40 @@ def main():
     print("Best selling games per platform:")
     platform_best_sellers = get_platform_best_sellers(vg_sales)
     print(platform_best_sellers.to_string(index=False))
+    #print(f"Best seller overall: {platform_best_sellers.loc[platform_best_sellers['Global Sales (in millions)'].idxmax()]}")
 
-    # Question 2: What year sold the most games?
-    #print(get_year_num_games(vg_sales).to_string())
+    # Question 2: What year sold the most games (out of games selling over 100k copies)?
     print("\nGlobal sales per year (of games with 100k sales):")
     year_global_sales = get_year_global_sales(vg_sales)
     print(year_global_sales.to_string(index=False))
+    print(f"Best selling year: {year_global_sales.loc[year_global_sales['Global Sales (in millions)'].idxmax()]}")
+
+    # Display a graph for global sales by year (pauses terminal)
+    graph_global_sales(year_global_sales)
 
     # Additional Questions
 
-    # Games that sold 100k+ per year
-    print("\nNumber of games selling 100k units per year:")
-    year_games = get_year_num_games(vg_sales)
-    print(year_games.to_string()) # Series, show index (Year)
-
     # Most popular genre for each year
-    print("\nMost popular genre per year:")
+    print("\nMost popular genre grouped by release year:")
     year_genre = get_genre_year(vg_sales)
     print(year_genre.to_string(index=False))
 
-    # Graphing (pauses terminal)
+    # Games that sold 100k+ per year
+    print("\nNumber of games selling 100k units, grouped by release year:")
+    year_games = get_year_num_games(vg_sales)
+    print(year_games.sort_values("Num Games", ascending=False).to_string(index=False))
+    #print(f"Total: {year_games['Games with 100k sales'].sum()}")
 
-    # Display a graph for global sales by year
-    graph_global_sales(year_global_sales)
+    # Display graph for successful games per release year (pauses terminal)
+    graph_game_per_year(year_games.sort_values("Year", ascending=False))
+
+    # Only including games that sold 1 million units
+    print("\nNumber of games selling 1 million units, grouped by release year:")
+    year_games_million = get_year_num_games_million(vg_sales)
+    print(year_games_million.sort_values("Num Games", ascending=False).to_string(index=False))
+
+    # Display graph for games selling 1 million, grouped by release year (pauses terminal)
+    graph_game_per_year_million(year_games_million.sort_values("Year", ascending=False))
 
 if __name__ == "__main__":
     main()
